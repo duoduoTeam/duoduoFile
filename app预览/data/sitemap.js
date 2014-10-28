424d15f81dccc0090bf2d88435dd16e52fc599a3
ba-jie <bajieat163@163.com>
2014-10-28 10:49:47 +0800

    x

--- app预览/data/sitemap.js
+++
@@ -1,8 +0,0 @@
-﻿var sitemap = 
-
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,[_(c,d,e,f,g,h,i,[_(c,j,e,f,g,k),_(c,l,e,f,g,m),_(c,n,e,f,g,o),_(c,p,e,f,g,q),_(c,r,e,f,g,s),_(c,t,e,f,g,u),_(c,v,e,f,g,w)])]);}; 
-var b="rootNodes",c="pageName",d="主页",e="type",f="Wireframe",g="url",h="主页.html",i="children",j="装修流程",k="装修流程.html",l="确定施工方式简介",m="确定施工方式简介.html",n="我",o="我.html",p="登陆页",q="登陆页.html",r="聊天",s="聊天.html",t="聊天列表",u="聊天列表.html",v="蓝景丽家",w="蓝景丽家.html";
-return _creator();
-})();
